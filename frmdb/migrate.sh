6fa767309fe2d66794e685265419ab00e268815f
acristu <alexandru.cristu@formuladb.io>
2021-03-20 12:05:45 +0200

    update executable flag

--- frmdb/migrate.sh
+++ frmdb/migrate.sh
@@ -3,16 +3,18 @@ set -ex
 while true; do find /frmdb | entr -d \
     'migrate_frmdb_db.sh && migrate_test_db.sh && migrate_bak_db.sh && migrate_dev_db.sh && migrate_postgres_db.sh'; 
     sleep 1
-done
+done &
 
 while true; do find /deploy/00_pg_dump.dir | entr -d \
     'migrate_bak_db.sh && migrate_dev_db.sh && migrate_postgres_db.sh'; 
     sleep 1
-done
+done &
 
 while true; do find /deploy/apps-resources | entr -d \
     'migrate_test_db.sh && migrate_dev_db.sh && migrate_postgres_db.sh'; 
     sleep 1
-done
+done &
 
-while true; do find /deploy/apps-pages | entr -d 'migrate_postgres_db_pages.sh'; sleep 1; done
+while true; do find /deploy/apps-pages | entr -d 'migrate_postgres_db_pages.sh'; sleep 1; done &
+
+wait
