d582e1904a33873c415c55ab7b0172a52b6da09b
acristu <alexandru.cristu@formuladb.io>
2021-03-22 14:40:30 +0200

    f

--- frmdb/migrate_bak_db.sh
+++ frmdb/migrate_bak_db.sh
@@ -2,11 +2,8 @@ set -ex
 . `dirname $0`/fct_utils.sh
 
 fctCreate_bak_db() {
-    until psql -h db -U postgres -lqt | cut -d \| -f 1 | grep -qw bak; do 
-        psql -h db -U postgres -c 'DROP DATABASE bak' || true
-        psql -v ON_ERROR_STOP=1 -h db -U postgres -c 'CREATE DATABASE bak WITH TEMPLATE frmdb'
-        sleep 2; 
-    done
+    psql -h db -U postgres -c 'DROP DATABASE bak' || true
+    psql -v ON_ERROR_STOP=1 -h db -U postgres -c 'CREATE DATABASE bak WITH TEMPLATE frmdb'
 
     if [ -d "/deploy/00_pg_dump.dir" ]; then
         time pg_restore -j3 -h db -U postgres -d bak --clean -Fd /deploy/00_pg_dump.dir || true
