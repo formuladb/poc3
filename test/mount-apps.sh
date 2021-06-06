5fd474a6239deb940b9807de25a5406437db1f05
acristu <alexandru.cristu@formuladb.io>
2021-06-06 16:26:36 +0300

    w

--- test/mount-apps.sh
+++ test/mount-apps.sh
@@ -1,2 +1,2 @@
 mkdir /mnt/wsl/prw-tenants
-sudo mount --bind $HOME/code/pagerows/core/resources/tenants /mnt/wsl/prw-tenants
+sudo mount --bind /home/acr/code/pagerows/pagerows/rows/tenants/ /mnt/wsl/prw-tenants
