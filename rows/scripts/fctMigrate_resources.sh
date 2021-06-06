8adb6b12d1b9c0268b773108b524960b5dd89b97
acristu <alexandru.cristu@formuladb.io>
2021-06-06 17:04:14 +0300

    embed via iframe into sfa

--- rows/scripts/fctMigrate_resources.sh
+++ rows/scripts/fctMigrate_resources.sh
@@ -13,20 +13,20 @@ fctMigrate_resources() {
             if [[ $i = *.test.sql ]]; then
                 if [ "$dbname" = "test" -o "$dbname" = "dev" ]; then
                     fctH2 "$dbname:$i"
-                    time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname -f $i
+                    time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname
                 else
                     echo "Skipping test file $i on $dbname";
                 fi
             elif [[ $i = *.data.sql ]]; then
                 if [ "$dbname" = "dev" ]; then
                     fctH2 "$dbname:$i"
-                    time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname -f $i
+                    time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname
                 else
                     echo "Skipping data file $i on $dbname";
                 fi
             else
                 fctH2 "$dbname:$i"
-                time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname -f $i
+                time psql -v ON_ERROR_STOP=1 -e -h db -U postgres -d $dbname
             fi
         done
         fctH2 "SUCCESS migrate resources for tentant ${tenant} on ${dbname} "
